67b8b05f3eee68db4a6ea973f22e3d9a156c6359
M3Andy1 <165163448+M3Andy1@users.noreply.github.com>
2024-03-27 13:46:47 +0100

    Add files via upload
    sad

--- Zinsen_calc.py
+++ Zinsen_calc.py
@@ -1,6 +1,14 @@
 #Test file
 
 import streamlit as st
+from PIL import Image
 
 st.write('This script helps to predict loan costs for fixed and variable interest rates')
-st.image('./chart.jpeg')
+
+ezb_image = st.file_uploader('./chart.jpeg', type='jpeg', key=6)
+if ezb_image is not None:
+    image = Image.open(ezb_image)
+    new_image = image.resize((600, 400))
+    st.image(new_image)
+
+
