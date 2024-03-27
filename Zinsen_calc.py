5f4351450a5779f864cbd4267748ffbb151ed2c8
M3Andy1 <165163448+M3Andy1@users.noreply.github.com>
2024-03-27 13:47:52 +0100

    Add files via upload
    gh

--- Zinsen_calc.py
+++ Zinsen_calc.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 st.write('This script helps to predict loan costs for fixed and variable interest rates')
 
-ezb_image = st.file_uploader('./chart.jpeg', type='jpeg', key=6)
+ezb_image = st.image('./chart.jpeg')
 if ezb_image is not None:
     image = Image.open(ezb_image)
     new_image = image.resize((600, 400))
