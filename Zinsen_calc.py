e15c2ab47e3dc2bd8441824c6345c5af461704d1
M3Andy1 <165163448+M3Andy1@users.noreply.github.com>
2024-03-27 18:17:25 +0100

    Add files via upload
    ds

--- Zinsen_calc.py
+++ Zinsen_calc.py
@@ -132,6 +132,6 @@ Laufzeit_list=np.linspace(0, Laufzeit-1, Laufzeit)
 
 
 
-df1 = pd.DataFrame({'Month': Laufzeit_list, 'Gesamtrueckzahlung Fix Zins': Gesamtrueckzahlung_fix, 'Gesamtrueckzahlung Variable Zins': Gesamtrueckzahlung_variable, 'Restwert Kredit': Kreditrestwert})
+df1 = pd.DataFrame({'Month': Laufzeit_list, 'Gesamtrueckzahlung Fix Zins': Gesamtrueckzahlung_fix,  'Restwert Kredit': Kreditrestwert})
 df1.set_index('Month', inplace=True)
 st.line_chart(df1)
